452ca877bf3c1a17f4e5fdd673b5f4e8de5c2b62
miquelcabot <miquel.cabot@gmail.com>
2025-02-10 20:55:23 +0100

    Refactor MultiUtilityNFT contract to use OpenZeppelin imports and add EIP712 support

--- src/MultiUtilityNFT.sol
+++ src/MultiUtilityNFT.sol
@@ -4,10 +4,10 @@ pragma solidity 0.8.20;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {EIP712} from "openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol";
-import {MerkleProof} from "openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 error ZeroAddress();
 error InvalidTimestamp();
@@ -15,7 +15,7 @@ error InvalidPhase();
 error InvalidProof();
 error AlreadyClaimed();
 
-contract MultiUtilityNFT is ERC721, Ownable, ReentrancyGuard {
+contract MultiUtilityNFT is ERC721, EIP712, Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     enum Phase {
@@ -51,7 +51,7 @@ contract MultiUtilityNFT is ERC721, Ownable, ReentrancyGuard {
         uint256 _phase1EndTimestamp,
         uint256 _phase2EndTimestamp,
         uint256 _phase3EndTimestamp
-    ) ERC721(name, symbol) Ownable(_owner) EIP712("MultiUtilityNFT", "1") {
+    ) ERC721(name, symbol) Ownable(_owner) EIP712(name, "1") {
         if (address(_paymentToken) == address(0)) revert ZeroAddress();
         if (_phase1EndTimestamp <= block.timestamp) revert InvalidTimestamp();
         if (_phase2EndTimestamp <= _phase1EndTimestamp) revert InvalidTimestamp();
