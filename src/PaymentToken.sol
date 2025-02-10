6429af445bfe5ec9b3d7005fb28d32f8288e1653
miquelcabot <miquel.cabot@gmail.com>
2025-02-10 15:33:25 +0100

    Add PaymentToken contract for testing purposes

---
+++ src/PaymentToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title PaymentToken
+ * @notice A simple ERC20 token for testing purposes
+ */
+contract PaymentToken is ERC20 {
+    /**
+     * @notice Construct a new PaymentToken
+     * @dev The token has the name "PaymentToken" and the symbol "PT"
+     */
+    constructor() ERC20("PaymentToken", "PT") {}
+
+    /**
+     * @notice Mint new tokens
+     * @dev Anyone can mint new tokens for testing purposes
+     * @param to Address to mint tokens to
+     * @param amount Amount of tokens to mint
+     */
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
