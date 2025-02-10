4d0179718ef60fae3e8d0e1bbffb365f1981282b
miquelcabot <miquel.cabot@gmail.com>
2025-02-10 15:05:38 +0100

    Rename to MultiUtilityNFT

---
+++ test/MultiUtilityNFT.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MultiUtilityNFT} from "../src/MultiUtilityNFT.sol";
+
+contract MultiUtilityNFTTest is Test {
+    MultiUtilityNFT public multiUtilityNFT;
+
+    function setUp() public {
+        multiUtilityNFT = new MultiUtilityNFT();
+        multiUtilityNFT.setNumber(0);
+    }
+
+    function test_Increment() public {
+        multiUtilityNFT.increment();
+        assertEq(multiUtilityNFT.number(), 1);
+    }
+
+    function testFuzz_SetNumber(uint256 x) public {
+        multiUtilityNFT.setNumber(x);
+        assertEq(multiUtilityNFT.number(), x);
+    }
+}
