25599abfbbbed659e8ed350883730a05b404dc61
miquelcabot <miquel.cabot@gmail.com>
2025-02-10 18:14:22 +0100

    Add Phase enumeration and current phase retrieval function

--- test/MultiUtilityNFT.t.sol
+++ test/MultiUtilityNFT.t.sol
@@ -9,6 +9,13 @@ contract MultiUtilityNFTTest is Test {
     MultiUtilityNFT public multiUtilityNFT;
     PaymentToken public paymentToken;
 
+    enum Phase {
+        Phase1,
+        Phase2,
+        Phase3,
+        Finished
+    }
+
     function setUp() public {
         paymentToken = new PaymentToken();
         multiUtilityNFT = new MultiUtilityNFT(
@@ -23,5 +30,18 @@ contract MultiUtilityNFTTest is Test {
             block.timestamp,
             block.timestamp
         );
+    }
+
+    function getCurrentPhase() public view returns (Phase) {
+        uint256 currentTimestamp = block.timestamp;
+        if (currentTimestamp <= multiUtilityNFT.phase1EndTimestamp()) {
+            return Phase.Phase1;
+        } else if (currentTimestamp <= multiUtilityNFT.phase2EndTimestamp()) {
+            return Phase.Phase2;
+        } else if (currentTimestamp <= multiUtilityNFT.phase3EndTimestamp()) {
+            return Phase.Phase3;
+        } else {
+            return Phase.Finished;
+        }
     }
 }
